08a8d636822fba68a5552ebb3c25881f4cc63ebd
Gustavo Faria Morette <88351614+Morettegustavo@users.noreply.github.com>
2022-09-16 21:40:48 -0300

    chore:study-heranca

---
+++ Lista4/src/main/java/br/edu/fatecfranca/lista3/lista4/Assistente.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.fatecfranca.lista3.lista4;
+
+/**
+ *
+ * @author 1090482113014
+ */
+public class Assistente {
+    
+}
---
+++ Lista4/src/main/java/br/edu/fatecfranca/lista3/lista4/Funcionario.java
@@ -0,0 +1,64 @@
+package br.edu.fatecfranca.lista3.lista4;
+public class Funcionario {
+    protected String nome, cpf;
+    protected int codigo;
+    protected float salario, cargaHoraria;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, String cpf, int codigo, float salario, float cargaHoraria) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.codigo = codigo;
+        this.salario = salario;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public float getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public void setCargaHoraria(float cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" + "nome=" + nome + ", cpf=" + cpf + ", codigo=" + codigo + ", salario=" + salario + ", cargaHoraria=" + cargaHoraria + '}';
+    }
+    
+      
+}
