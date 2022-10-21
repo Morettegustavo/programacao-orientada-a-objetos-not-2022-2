a0c7cfb4dbe09b6dc6cb94d38833e757fd862693
Gustavo Faria Morette <88351614+Morettegustavo@users.noreply.github.com>
2022-10-21 19:41:26 -0300

    chore:add_interface

---
+++ Lista5/src/main/java/br/edu/fatecfranca/lista3/lista5/IMamifero.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.edu.fatecfranca.lista3.lista5;
+
+/**
+ *
+ * @author 1090482113014
+ */
+public interface IMamifero {
+    
+}
---
+++ Lista5/src/main/java/br/edu/fatecfranca/lista3/lista5/Lista5.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package br.edu.fatecfranca.lista3.lista5;
+
+/**
+ *
+ * @author 1090482113014
+ */
+public class Lista5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
